d2d4a695f6abb98aee762f6f49b2b801f4f15c20
Roy C <v0o0605@gmail.com>
2023-01-20 01:09:55 +0900

    JPA 연습시 initial data insert 를 위한 file

---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into member ('id', 'name', 'email', 'created_at', 'updated_at') values (1, 'Roy', 'roy@naver.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into member ('id', 'name', 'email', 'created_at', 'updated_at') values (2, 'Olivia', 'olivia@naver.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into member ('id', 'name', 'email', 'created_at', 'updated_at') values (3, 'Mari', 'mari@naver.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into member ('id', 'name', 'email', 'created_at', 'updated_at') values (4, 'Collin', 'collin@naver.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into member ('id', 'name', 'email', 'created_at', 'updated_at') values (5, 'Roy', 'rrrroyc@naver.com', now(), now());
