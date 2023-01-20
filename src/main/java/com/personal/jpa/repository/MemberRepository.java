62869f280d3765dd96f75cf3fc8b0c95645318f3
Roy C <v0o0605@gmail.com>
2023-01-21 00:35:39 +0900

    JPA query method - sorting

--- src/main/java/com/personal/jpa/repository/MemberRepository.java
+++ src/main/java/com/personal/jpa/repository/MemberRepository.java
@@ -1,18 +1,37 @@
 package com.personal.jpa.repository;
 
 import com.personal.jpa.domain.Member;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByName(String name);
     Member findByEmail(String email);
-
     Member findMemberById(Long id);
-
     List<Member> findAllByName(String name);
-
     List<Member> findMembersByEmail(String email);
+    List<Member> findByNameAndEmail(String name, String email);
+    List<Member> findByNameOrEmail(String name, String email);
+    List<Member> findByCreatedAtAfter(LocalDateTime yesterday);
+    List<Member> findByIdAfter(Long id);
+    List<Member> findByCreatedAtGreaterThan(LocalDateTime yesterday);
+    List<Member> findByCreatedAtGreaterThanEqual(LocalDateTime yesterday);
+    List<Member> findByCreatedAtBetween(LocalDateTime yesterday, LocalDateTime tomorrow);
+    List<Member> findByIdBetween(Long id1, Long id2);
+    List<Member> findByIdGreaterThanEqualAndIdLessThanEqual(Long id1, Long id2);
+    List<Member> findByIdIsNotNull();
+    List<Member> findByNameIn(List<String> names);
+    List<Member> findByNameStartingWith(String name);
+    List<Member> findByNameEndingWith(String name);
+    List<Member> findByNameContains(String name);
+    List<Member> findByNameLike(String name);
+
+    List<Member> findTopByName(String name);
+    List<Member> findTopByNameOrderByIdDesc(String name);
+    List<Member> findFirstByNameOrderByIdDescEmailAsc(String name);
+    List<Member> findFirstByName(String name, Sort sort);
 }
--- src/test/java/com/personal/jpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/personal/jpa/repository/MemberRepositoryTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.*;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Sort.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
@@ -71,5 +72,51 @@ class MemberRepositoryTest {
         memberRepository.findByEmail("roy@naver.com");
         memberRepository.findMemberById(3L);
         memberRepository.findMembersByEmail("mari@naver.com");
+
+        // 두가지 조건 모두 충족
+        memberRepository.findByNameAndEmail("Roy", "roy@naver.com");
+        // 두가지중 한가지 조건 충족
+        memberRepository.findByNameOrEmail("Roy", "1111@naver.com");
+        // 시간에 대한 조건 충족
+        memberRepository.findByCreatedAtAfter(LocalDateTime.now().minusDays(1L));   // 현재 시간에서 하루 전
+        // After 와 Before 는 단순 숫자로 비교해 동작 가능하지만 가독성을 위해서 되도록이면 날짜와 시간의 경우에만 사용하는것을 권장
+        // created_at > 2
+        memberRepository.findByIdAfter(2L);
+        // GreaterThan 은 모든 숫자값,날짜값 등에 범용적으로 사용하기 좋음
+        // created_at > time
+        memberRepository.findByCreatedAtGreaterThan(LocalDateTime.now().minusDays(1L));
+        // created_at >= time
+        memberRepository.findByCreatedAtGreaterThanEqual(LocalDateTime.now().minusDays(1L));
+
+        memberRepository.findByCreatedAtBetween(LocalDateTime.now().minusDays(1L), LocalDateTime.now().plusDays(1L));
+        // between 은 양 조건을 포함 하는 결과를 가져옴
+        memberRepository.findByIdBetween(2L, 3L);
+        // between 쿼리와 동일한 조건
+        memberRepository.findByIdGreaterThanEqualAndIdLessThanEqual(1L, 3L);
+
+        memberRepository.findByIdIsNotNull();
+        // List 의 name 들을 포함하는 name 을 갖고있는 결과값 조회, Or 과 비슷하게 동작
+        memberRepository.findByNameIn(Lists.newArrayList("Roy", "Olivia"));
+
+        // Oli 문자의 앞뒤로 양방향 like 검색
+        memberRepository.findByNameLike("%Oli%");
+        // 밑의 3가지는 위의 Like method 를 한번더 mapping 한것과 같다
+        memberRepository.findByNameStartingWith("Mari");
+        memberRepository.findByNameEndingWith("lin");
+        memberRepository.findByNameContains("ivi");
+    }
+
+    void pagingAndSortingTesy() {
+
+        // 첫번째 Collin 을 조회
+        memberRepository.findTopByName("Collin");
+        // 역순으로 정렬하여 첫번째 Collin 을 조회.  = 마지막 Collin
+        memberRepository.findTopByNameOrderByIdDesc("Collin");
+        // Id 는 역순, Email 은 정순으로 정렬하여 첫번째 Roy 조회    // OrderBy 뒤에는 And 를 쓰지않고 요소를 나열함
+        memberRepository.findFirstByNameOrderByIdDescEmailAsc("Roy");
+        // 위의 조건과 동일한 정렬, 검색    // Sort class 를 활용하여 추가적인 paramegter 제공
+        memberRepository.findFirstByName("Roy", Sort.by(Order.desc("id"), Order.asc("email")));
+
+
     }
 }
