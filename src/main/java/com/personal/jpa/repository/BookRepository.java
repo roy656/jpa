382c607dd0d779007330bc720f85c30548e04292
Roy C <v0o0605@gmail.com>
2023-01-22 22:42:35 +0900

    JPA Entity Listener 의 7가지 Annotations 사용

---
+++ src/main/java/com/personal/jpa/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.personal.jpa.repository;
+
+import com.personal.jpa.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+
+}
